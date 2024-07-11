b19897d4bc38bb8822e465af64d2e234588b034b
HuangErGou <ergou.huang@hotmail.com>
2024-07-11 20:41:24 +0800

    can basic search

--- PlayGround/test.py
+++ PlayGround/test.py
@@ -7,20 +7,41 @@ from pydantic.v1 import BaseModel, Field
 from LLM.NagasenaLLM import NagasenaLLM
 from langchain.agents import create_openai_tools_agent, AgentExecutor
 
+import shlex
+import subprocess
+
+
+def execute_command(command_str: str) -> str:
+    # 使用shlex.split()分割命令字符串
+    command = shlex.split(command_str)
+
+    # 现在command是一个列表，包含了命令和参数
+    print(command)
+    # 使用subprocess.run()执行命令
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True, errors="ignore")
+        output = result.stdout
+        print("Command output:")
+        print(output)
+        return output
+    except subprocess.CalledProcessError as e:
+        print("An error occurred while executing the command.")
+        print(e)
+
 
 class SearchInput(BaseModel):
-    sentence: str = Field(description="用来搜索的字符串, 字符串需要翻译成英文")
+    command: str = Field(description="生成的搜索命令")
 
 
 class SearchTool(BaseTool):
     name = "SearchTool"
-    description = "github 搜索工具，可以在github 上搜索仓库"
+    description = "命令执行工具，可以执行命令"
     args_schema: Type[BaseModel] = SearchInput
 
-    def _run(self, sentence: str) -> str:
+    def _run(self, command: str) -> str:
         """Use the tool."""
-        print(f"call function search {sentence}")
-        return "没有找到"
+        print(f"call function search {command}")
+        return execute_command(command)
 
 
 prompt = hub.pull("hwchase17/openai-tools-agent")
@@ -30,9 +51,17 @@ llm = NagasenaLLM(temperature=0)
 agent = create_openai_tools_agent(llm=llm, tools=tools, prompt=prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
+search_input = "langchain gui"
+prompt = f"github client 搜索仓库的例子如下：\n \
+          \\# search repositories matching set of keywords \"cli\" and \"shell\" \n \
+          $ gh search repos cli shell \n \
+          # search repositories matching phrase \"vim plugin\" \n \
+          $ gh search repos \"vim plugin\"\n \" \
+          请参考这些例子生成搜索命令，调用命令执行工具去找{search_input}相关的仓库"
+
 response = agent_executor.invoke(
     {
-        "input": "抽取'基于langchain的JIRA附件下载'里面的关键字, 在github上搜索, 如果没有找到"
+        "input": prompt
     }
 )
 print(response)
