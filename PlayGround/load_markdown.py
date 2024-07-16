b5a28b7afe6cd96152f740881782586eacc572f8
HuangErGou <ergou.huang@hotmail.com>
2024-07-16 17:18:10 +0800

    add markdown retrive play ground

--- PlayGround/load_markdown.py
+++ PlayGround/load_markdown.py
@@ -1,36 +1,48 @@
-from langchain_community.document_loaders import UnstructuredMarkdownLoader
+from langchain.retrievers import EnsembleRetriever
 from langchain_community.vectorstores import FAISS
-from langchain_core.documents import Document
-from langchain_text_splitters import CharacterTextSplitter, MarkdownTextSplitter
+from langchain_text_splitters import MarkdownHeaderTextSplitter, RecursiveCharacterTextSplitter
 from langchain_community.retrievers import BM25Retriever
 
 from Embedding.NagasenaEmbeddings import NagasenaEmbeddings
 
 markdown_path = "D:\\test\\minigui_readme.md"
 
-loader = UnstructuredMarkdownLoader(markdown_path)
-documents = loader.load()
+with open(file=markdown_path, encoding="utf-8") as f:
+    content = f.read()
 
-
-text_splitter = MarkdownTextSplitter()
-
-texts = text_splitter.split_documents(documents)
-
-
-doc_list_1 = [
-    "light weight window manage system"
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
 ]
-bm25_retriever = BM25Retriever.from_documents(texts)
-docs = bm25_retriever.invoke("light weight window manage system")
+
+markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+md_header_splits = markdown_splitter.split_text(content)
+
+chunk_size = 250
+chunk_overlap = 30
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=chunk_size, chunk_overlap=chunk_overlap
+)
+
+# Split
+split = text_splitter.split_documents(md_header_splits)
+
+
+embeddings = NagasenaEmbeddings()
+vectorstore = FAISS.from_documents(split, embeddings)
+
+faiss_retriever = vectorstore.as_retriever(search_kwargs={"k": 2})
+
+bm25_retriever = BM25Retriever.from_documents(split)
+bm25_retriever.k = 2
+
+ensemble_retriever = EnsembleRetriever(
+    retrievers=[bm25_retriever, faiss_retriever], weights=[0.5, 0.5]
+)
+
+query = "gui can run no vxworks"
+docs = ensemble_retriever.invoke(query)
+
+
 print(docs)
-
-
-# print("111111")
-# embeddings = NagasenaEmbeddings()
-# vectorstore = FAISS.from_documents(texts, embeddings)
-#
-# retriever = vectorstore.as_retriever()
-#
-# docs = retriever.invoke("linux window")
-# print(docs)
-# print(len(docs[0].page_content))
